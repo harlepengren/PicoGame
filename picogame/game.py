909058ae9c4a8edb53d064751512fd6710aa780d
kkoehler4070 <koehler.62@gmail.com>
2023-04-28 19:34:19 -0400

    Timers
    Updated the timer for more precision.

--- picogame/game.py
+++ picogame/game.py
@@ -1,6 +1,7 @@
 from . import screen
 from . import image
-from machine import Timer, Pin
+from machine import Pin
+import time
 
 class PicoGame:
     def __init__(self):
@@ -13,37 +14,55 @@ class PicoGame:
         self.x = 0
         self.y = 30
 
-    def draw(self):
+    def draw(self,showFPS=False):
         self.x += 1
         
         if self.x > 128:
             self.x = 0
         
         self._screen.fill(0x00)
-        self._screen.blit(self._spriteBuffer,x,y)
+        self._screen.blit(self._spriteBuffer,self.x,self.y)
+        
+        if(showFPS):
+            deltaTime = time.ticks_diff(self.currentTime,self.lastTime)
+            currentFPS = 1/deltaTime
+            self._screen.text(str(currentFPS),110,110,0xFF)
+        
+        
         self._screen.show()
 
-    def run(self):
-        # Set up the timer
-        timer = Timer()
-        timer.init(mode=Timer.PERIODIC, freq=60, callback=self.draw)
+    def run(self,fps=60):
         
         # Set up the input pins
         key0 = Pin(15, Pin.IN, Pin.PULL_UP)
         key1 = Pin(17, Pin.IN, Pin.PULL_UP)
         key2 = Pin(2, Pin.IN, Pin.PULL_UP)
         
+        # Run initial draw
+        self.draw()
+        
+        # Set up the timer
+        self.lastTime = time.ticks_ms()
+        secondsPerFrame = 1/fps
+        
         while(True):
-            if not key0:
+            self.currentTime = time.ticks_ms()
+            
+            if time.ticks_diff(self.currentTime,self.lastTime) > secondsPerFrame:
+                self.draw(True)
+                self.lastTime = self.currentTime
+            
+            if not key0.value():
                 # we received the exit button
-                timer.deinit()
                 break
             
-            if not key2:
+            if not key2.value():
                 x -= 5
                 
-            if not key1:
+            if not key1.value():
                 x += 5
+        
+        
             
             
         
