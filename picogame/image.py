266be4799d49bec6795619a88773f3b7263d6694
kkoehler4070 <koehler.62@gmail.com>
2023-04-28 04:41:37 -0400

    Image Updates
    Removed struct since it is not available on MicroPython and added int.form_bytes instead. Also added a method to turn an image into a frame buffer for later processing.

--- picogame/image.py
+++ picogame/image.py
@@ -1,26 +1,33 @@
 import os
-import struct
+import framebuf
+
+SEEK_CUR = 1
 
 def openImage(path):
     with open(path, mode='rb') as f:
-        signature = struct.unpack('2c', f.read(2))
-        fsize = struct.unpack('I',f.read(4))
-        f.seek(4,os.SEEK_CUR)
-        dataOffset = struct.unpack('I',f.read(4))[0]
+        signature = f.read(2)
+        fsize = int.from_bytes(f.read(4),'little')
+        f.seek(4,SEEK_CUR)
+        dataOffset = int.from_bytes(f.read(4),'little')
 
-        infoHeaderSize = struct.unpack('I',f.read(4))
-        size = struct.unpack('2I',f.read(8))
-        f.seek(2,os.SEEK_CUR)
-        depth = struct.unpack('H',f.read(2))[0]
+        infoHeaderSize = int.from_bytes(f.read(4),'little')
+        size = (int.from_bytes(f.read(4),'little'),
+                int.from_bytes(f.read(4),'little'))
+        f.seek(2,SEEK_CUR)
+        depth = int.from_bytes(f.read(2),'little')
 
         # If compression is 3 then we also need the mask fields to determine whether it is RGB555 or RGB565
-        compression = struct.unpack('I',f.read(4))
-        imageSize = struct.unpack('I', f.read(4))
+        compression = int.from_bytes(f.read(4),'little')
+        imageSize = int.from_bytes(f.read(4),'little')
 
-        f.seek(4,os.SEEK_CUR)
+        f.seek(4,SEEK_CUR)
 
-        colorUsed = struct.unpack('2I', f.read(8))
-        masks = struct.unpack('<4I',f.read(16))
+        #colorUsed = struct.unpack('2I', f.read(8))
+        f.seek(8, SEEK_CUR)
+        masks = (int.from_bytes(f.read(4),'little'),
+                 int.from_bytes(f.read(4),'little'),
+                 int.from_bytes(f.read(4),'little'),
+                 int.from_bytes(f.read(4),'little'))
 
         # Go to end of head
         f.seek(dataOffset)
@@ -28,11 +35,12 @@ def openImage(path):
 
         rowSize = int(size[0]*depth/32*4)
 
-        imageData = []
-        for currentRow in range(0,size[1]):
-            currentRead = f.read(rowSize)
-            if len(currentRead) > 0:
-                imageData.append(currentRead)
+        imageData = framebuf.FrameBuffer(f.read(rowSize*size[1]), size[0], size[1], framebuf.RGB565)
+        #imageData = []
+        #for currentRow in range(0,size[1]):
+        #    currentRead = f.read(rowSize)
+        #    if len(currentRead) > 0:
+        #        imageData.append(currentRead)
 
 
         print("Signature: ", signature)
@@ -43,11 +51,22 @@ def openImage(path):
         print("Bit Depth: ", depth)
         print("Compression: ", compression)
         print("Image Size: ", imageSize)
-        print("Color Table Used: ", colorUsed)
+        #print("Color Table Used: ", colorUsed)
         print("Masks: ", masks)
         print("Row Size: ", rowSize)
         print("Number of rows: ", len(imageData))
 
-        print("First row: ", imageData[0])
+        return (size,imageData)
+    
+def createImageBuffer(self,sprite,x,y):
+    #self.blit(sprite,size,x,y)
+    width = size[0]
+    height = size[1]
+    tempBuffer = bytearray(width*height*2)
+    buffer = framebuf.FrameBuffer(tempBuffer,width,height,framebuf.RGB565)
+    
+    for y in range(0,height):
+        for x in range(0, width):
+            buffer.pixel(x,y,sprite[x*y*2])
 
-        return imageData
+    return buffer
