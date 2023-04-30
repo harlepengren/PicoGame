45fe5fd1858f45f20faa2e1b6527850107080b8e
kkoehler4070 <koehler.62@gmail.com>
2023-04-30 08:39:51 -0400

    Update vector.py
    Added ability to intake a tuple. Also simplified some vector math.

--- picogame/vector.py
+++ picogame/vector.py
@@ -1,18 +1,14 @@
 class Vector:
     def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+        if isinstance(x,tuple):
+            self.x = x[0]
+            self.y = y[0]
+        else:
+            self.x = x
+            self.y = y
         
     def __add__(self, other):
-        newVector = Vector()
-        newVector.x = self.x + other.x
-        newVector.y = self.y + other.y
-        return newVector
-    
-    def __subtract__(self, other):
-        newVector = Vector()
-        newVector.x = self.x - other.x
-        newVector.y = self.y - other.y
+        newVector = Vector(self.x+other.x,self.y+other.y)
         return newVector
     
     def __mul__(self, scalar:int):
