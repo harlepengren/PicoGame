b61bb5050240bc7c9bf2f80980fe6e420c33e0c6
kkoehler4070 <koehler.62@gmail.com>
2023-05-01 06:21:52 -0400

    Update test.py
    Updated the way that projectiles are created. Created periodic timer that calls a createProjectile callback.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from picogame.image import Image
 from picogame.input import Input
 from picogame.vector import Vector
 from random import randint
-
+from machine import Timer
 
 class MyGame(GameController):
     def __init__(self):
@@ -35,6 +35,14 @@ class MyGame(GameController):
             . . . . f f f f f f f f f f . .""")
         self.player = Sprite(SpriteKind.PLAYER, image=playerImage)
         
+        self.playerInput = Input()
+        
+        # Create timer for projectiles
+        projectileTimer = Timer()
+        projectileTimer.init(mode=Timer.PERIODIC, period=1500, callback=self.createProjectile)
+        
+        
+    def createProjectile(self,timerObject):
         # Projectile
         projectileImage = Image()
         projectileImage.createFromString("""
@@ -54,21 +62,18 @@ class MyGame(GameController):
                     . f 1 f 1 f 1 f 1 f 1 f 1 1 f . 
                     . . f 1 f 1 f 1 f 1 f 1 f f . . 
                     . . . f f f f f f f f f f . . .""")
-        projectile = Sprite(SpriteKind.PROJECTILE,position=(128,randint(0,128)),speed=(0,-10),image=projectileImage)
+        projectile = Sprite(SpriteKind.PROJECTILE,position=(128,randint(0,128)),speed=(-1,0),image=projectileImage)
         self.projectiles.append(projectile)
-        
-        self.playerInput = Input()
-        
     
     def update(self):
-        super().update()
+        self.clearScreen()
         
-        # Get player input
-        currentY = self.playerInput.getY()
-        if currentY < 0:
+        # Get player input - for now, I am using A and B buttons
+        #currentY = self.playerInput.getY()
+        if self.playerInput.getA():
             self.player.position += Vector(0,5)
             
-        if currentY > 0:
+        if self.playerInput.getB():
             self.player.position += Vector(0,-5)
         
         # Update enemy and projectile positions
