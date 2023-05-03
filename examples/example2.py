dad1f684fc33a3fc2fbf9881e5aa2a663138113d
kkoehler4070 <koehler.62@gmail.com>
2023-05-03 06:10:23 -0400

    Create example2.py
    Created example 2, which is based on sample 1, but adds collision detection.

---
+++ examples/example2.py
@@ -0,0 +1,103 @@
+from picogame.game import GameController
+from picogame.sprite import Sprite, SpriteKind
+from picogame.image import Image
+from picogame.input import Input
+from picogame.vector import Vector
+from picogame import collision
+from random import randint
+from machine import Timer
+
+class MyGame(GameController):
+    def __init__(self):
+        super().__init__()
+        
+        # Create my sprites
+        self.projectiles = []
+        self.enemies = []
+        
+        # The player
+        playerImage = Image()
+        playerImage.createFromString("""
+        . . . . . . f f f f f f . . . . 
+            . . . . . f 1 1 1 1 1 1 f . . . 
+            . . . . f 1 1 1 1 1 1 1 1 f . . 
+            . . . f 1 f 1 1 1 1 1 1 1 1 f . 
+            . . . f f 1 1 1 1 1 1 1 1 1 f . 
+            . . . f 1 f 1 1 f 1 1 f 1 1 f . 
+            . . . f f 1 1 1 1 1 1 1 1 1 f . 
+            . . f f 1 f 1 f 1 1 1 1 1 f . . 
+            . f 1 1 f 1 f 1 f f f f f . . . 
+            f 1 1 1 f f f 1 1 1 1 1 1 f . . 
+            f f 1 f 1 f 1 f f 1 1 1 1 1 f . 
+            . . f 1 f 1 f 1 f 1 1 1 1 1 f f 
+            . . f f 1 f 1 f 1 f 1 1 1 f 1 f 
+            . . f f f 1 f 1 f 1 f 1 f 1 f f 
+            . . . f 1 f 1 f 1 f 1 f 1 f f . 
+            . . . . f f f f f f f f f f . .""")
+        self.player = Sprite(SpriteKind.PLAYER, image=playerImage)
+        
+        self.playerInput = Input()
+        
+        # Create timer for projectiles
+        projectileTimer = Timer()
+        projectileTimer.init(mode=Timer.PERIODIC, period=1500, callback=self.createProjectile)
+        
+        
+    def createProjectile(self,timerObject):
+        # Projectile
+        projectileImage = Image()
+        projectileImage.createFromString("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . f f f f f f . . . . 
+                    . . . . . f f f 1 1 1 1 f . . . 
+                    . . . . f f 1 1 f f 1 f 1 f . . 
+                    . . . . f 1 f 1 f f 1 f 1 1 f . 
+                    . f f f f f 1 f 1 1 1 1 f 1 f . 
+                    f 1 1 1 f f f 1 f 1 1 f 1 f 1 f 
+                    f f 1 1 1 1 f f 1 f f 1 f 1 f . 
+                    f 1 f 1 1 1 1 f f 1 1 1 1 f . . 
+                    f f 1 f 1 1 1 f 1 1 1 1 1 1 f . 
+                    f 1 f 1 f f f 1 f 1 1 1 1 1 f . 
+                    . f 1 f 1 f 1 f 1 f 1 f 1 1 f . 
+                    . . f 1 f 1 f 1 f 1 f 1 f f . . 
+                    . . . f f f f f f f f f f . . .""")
+        projectile = Sprite(SpriteKind.PROJECTILE,position=(128,randint(0,128)),speed=(-2,0),image=projectileImage)
+        self.projectiles.append(projectile)
+    
+    def update(self):
+        self.clearScreen()
+        
+        # Get player input - for now, I am using A and B buttons
+        #currentY = self.playerInput.getY()
+        if self.playerInput.getA():
+            self.player.position += Vector(0,5)
+            
+        if self.playerInput.getB():
+            self.player.position += Vector(0,-5)
+        
+        # Update enemy and projectile positions
+        for currentProjectile in self.projectiles:
+            currentProjectile.updatePosition()
+            
+        for currentEnemy in self.enemies:
+            currentEnemy.updatePosition()
+            
+        # Collision detection
+        collisionList = collision.findColliders(self.player,self.projectiles)
+            
+        
+        # Draw everything
+        for currentProjectile in self.projectiles:
+            self.blit(currentProjectile)
+            
+        for currentEnemy in self.enemies:
+            self.blit(currentEnemy)
+            
+        self.blit(self.player)
+            
+            
+if __name__ == '__main__':
+    myGame = MyGame()
+    myGame.run()
