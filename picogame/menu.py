cd3b2f21334c0f98365e15a2231f199498c75f41
kkoehler4070 <koehler.62@gmail.com>
2023-05-09 09:49:31 -0400

    Create menu.py
    Create script to load files. Started with basic sd (SPI) code.

---
+++ picogame/menu.py
@@ -0,0 +1,19 @@
+# Loads a list of games from SD card and allows user to select file
+import machine
+import sdcard
+import uos
+
+def initCard():
+    cs = machine.Pin(9, machine.Pin.OUT)
+    spi = machine.SPI(1, baudrate=1000000,
+                      firstbit=machine.SPI.MSB,
+                      sck=machine.Pin(10),
+                      mosi=machine.Pin(11),
+                      miso=machine.Pin(8))
+    
+    sd = sdcard.SDCard(spi, cs)
+    vfs = uos.VfsFat(sd)
+    uos.mount(vfs, "/games")
+
+def loadGames():
+    gameList = uos.listdir
