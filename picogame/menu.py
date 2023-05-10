e4b3e5351aaf047f7bd22f586aff1db8734f6681
kkoehler4070 <koehler.62@gmail.com>
2023-05-10 07:20:16 -0400

    Update menu.py
    Moved function to mount a card to sdcard. Added a menu class that is a subclass of gamecontroller.

--- picogame/menu.py
+++ picogame/menu.py
@@ -1,20 +1,41 @@
-# Loads a list of games from SD card and allows user to select file
+"""
+Loads a list of games from SD card and allows user to select file.
+"""
+
 import machine
-import sdcard
-import uos
+from . import sdcard
+from . import game
+import os
 
-def initCard():
-    cs = machine.Pin(9, machine.Pin.OUT)
-    spi = machine.SPI(1, baudrate=1000000,
-                      firstbit=machine.SPI.MSB,
-                      sck=machine.Pin(10),
-                      mosi=machine.Pin(11),
-                      miso=machine.Pin(8))
+# Load the games from the /games director as a list
+# Iterate through list and print the names on the screen
+# Add a selection bullet that shows the currently selected
+# If user selects a game, load the game
+
+class Menu(GameController):
+    def __init__(self,path):
+        self.path = path
+        self.loadGames(path)
     
-    sd = sdcard.SDCard(spi, cs)
-    vfs = uos.VfsFat(sd)
-    uos.mount(vfs, '/games')
+    def loadGames(self,path):
+        """Load games from path. This function will only return games with .py extension."""
+        gameList = os.listdir(path)
+        
+        # Process the list to remove filename extensions
+        deletionList = []
+        for index in range(0,len(gameList)):
+            extension = gameList[index].find('.py')
+            if extension > 0:
+                gameList[index] = gameList[index][0:extension]
+            else:
+                # Not a python file, add to deletion list
+                deletionList.append(gameList[index])
+                
+        for file in deletionList:
+            gameList.remove(file)
+            
+        self.gameList = gameList
 
-def loadGames():
-    gameList = uos.listdir('/games')
-    return gameList
+    def showGames(self):
+        """Show a list of games on the screen."""
+        pass
