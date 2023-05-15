499cf0c0c77c77c88187debb7c81fd65552be054
kkoehler4070 <koehler.62@gmail.com>
2023-05-15 14:19:01 -0400

    Update makecode.py
    Continued adding blocks

--- picogame/makecode.py
+++ picogame/makecode.py
@@ -1,6 +1,7 @@
 from .controller import *
 import sprite, image
 import game as gme
+import random
 
 # Create controller
 class MakeCodeInput(Input):
@@ -13,18 +14,37 @@ class MakeCodeInput(Input):
         del self.controls["A"]
         del self.Controls["B"]
 
+    def move_sprite(self,currentSprite):
+        pass
+
 controller = MakeCodeInput()
+
+class Scene:
+    def set_background_image(self,backgroundImage):
+        pass
 
 class Sprites:
     def __init__(self):
         self._spriteList = []
         self.overlaps = []
 
+    def create(self,spriteImage,spriteKind):
+        newSprite = sprite.Sprite(spriteKind,position=(64,64),image=spriteImage)
+        self._spriteList.append(newSprite)
+
+        return newSprite
+
     def create_projectile_from_sprite(self,spriteImage,fromSprite,spriteX,spriteY):
         newSprite = sprite.Sprite(sprite.SpriteKind.PROJECTILE,speed=(0,0),image=spriteImage)
         newSprite.fromSprite = fromSprite
 
         self._spriteList.append(newSprite)
+
+        return newSprite
+
+    def create_projectile_from_side(self,newImage,vx,vy):
+        newSprite = sprite.Sprite(sprite.SpriteKind.PROJECTILE,speed=(vx,vy),image=newImage)
+
 
     # Add handler for overlaps
     def on_overlap(self, spriteKind1,spriteKind2,callback):
@@ -41,8 +61,26 @@ sprites = Sprites()
 
 # Game Controller
 class MakeCodeController(gme.GameController):
-    def reset():
+    def __init__(self):
+        super.__init__()
+        self.intervalUpdates = []
+    
+    def update(self):
+        # Make sure to check whether destroyed flag is set and handle
+        
+        # check whether time as passed
+        for currentUpdate in self.intervalUpdates:
+            if self.deltaTime > currentUpdate[0]:
+                currentUpdate[1]()
+    
+    def on_update_interval(self, interval,callback):
+        self.intervalUpdates.append((interval,callback))
+
+    def reset(self):
         # To do in the future
         pass
 
 game = MakeCodeController()
+
+def randint(start,stop):
+    return random.randint(start,stop)
