ea3676ee9741d4725ec0690a3fced72cc31d9854
kkoehler4070 <koehler.62@gmail.com>
2023-05-15 07:20:43 -0400

    Update makecode.py
    significant updates

--- picogame/makecode.py
+++ picogame/makecode.py
@@ -1,4 +1,6 @@
 from .controller import *
+import sprite, image
+import game as gme
 
 # Create controller
 class MakeCodeInput(Input):
@@ -12,3 +14,35 @@ class MakeCodeInput(Input):
         del self.Controls["B"]
 
 controller = MakeCodeInput()
+
+class Sprites:
+    def __init__(self):
+        self._spriteList = []
+        self.overlaps = []
+
+    def create_projectile_from_sprite(self,spriteImage,fromSprite,spriteX,spriteY):
+        newSprite = sprite.Sprite(sprite.SpriteKind.PROJECTILE,speed=(0,0),image=spriteImage)
+        newSprite.fromSprite = fromSprite
+
+        self._spriteList.append(newSprite)
+
+    # Add handler for overlaps
+    def on_overlap(self, spriteKind1,spriteKind2,callback):
+        self.overlaps.append((spriteKind1,spriteKind2,callback))
+
+    def destroy(self,currSprite):
+        self._spriteList.remove(currSprite)
+
+def img(newImage):
+    return image.Image().createFromString(newImage)
+
+sprites = Sprites()
+
+
+# Game Controller
+class MakeCodeController(gme.GameController):
+    def reset():
+        # To do in the future
+        pass
+
+game = MakeCodeController()
