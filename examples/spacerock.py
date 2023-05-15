fcb9ae1db57e3f897116ec0d612ad584760ceb0d
kkoehler4070 <koehler.62@gmail.com>
2023-05-15 07:20:04 -0400

    Update spacerock.py

--- examples/spacerock.py
+++ examples/spacerock.py
@@ -1,6 +1,23 @@
 from picogame.makecode import *
+from picogame.sprite import *
 
 def on_a_pressed():
     global projectile
-
+    projectile = sprites.create_projectile_from_sprite(img("""
+            . d . 
+                    . d . 
+                    . d . 
+                    . . . 
+                    . d . 
+                    d . d 
+                    d . d 
+                    . d . 
+                    . . . 
+                    . d . 
+                    . d . 
+                    . d .
+        """),
+        myShip,
+        0,
+        -150)
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
