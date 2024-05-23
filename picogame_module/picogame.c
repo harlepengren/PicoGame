3d40427098f0b4890e10367fd73f39fe615c55f0
kkoehler4070 <koehler.62@gmail.com>
2024-05-23 05:57:49 -0400

    Added picogame module

---
+++ picogame_module/picogame.c
@@ -0,0 +1,11 @@
+#include "picogame.h"
+
+// Define the python references
+//static MP_DEFINE_COST_FUN_OBJ_X...
+
+// Define all attributes of the module.
+// Table entries are key/value pairs of the attribute name (a string)
+// and the MicroPython object reference.
+// All identifiers and strings are written as MP_QSTR_xxx and will be
+// optimized to word-sized integers by the build system (interned strings)
+//static const mp_rom_map_elem_t ...
