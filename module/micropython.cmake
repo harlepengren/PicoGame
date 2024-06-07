598511cb99a6db03609d3365cc00562a0918fdd0
kkoehler4070 <koehler.62@gmail.com>
2024-06-07 05:31:08 -0400

    Test function
    Added a test function to module to test in micropython.

--- module/micropython.cmake
+++ module/micropython.cmake
@@ -1,11 +1,11 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/picogame_sdk)
+
 # Create an INTERFACE library for our C module.
 add_library(picogame INTERFACE)
 
-include(${CMAKE_CURRENT_LIST_DIR}/../picogame_sdk/picogame_sdk.cmake)
-
 # Add our source files to the lib
 target_sources(picogame INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/picogame.c #${CMAKE_CURRENT_LIST_DIR}/simplemath.c
+    ${CMAKE_CURRENT_LIST_DIR}/picogame.c ${CMAKE_CURRENT_LIST_DIR}picoscreen.cpp
 )
 
 # Add the current directory as an include directory.
@@ -14,4 +14,4 @@ target_include_directories(picogame INTERFACE
 )
 
 # Link our INTERFACE library to the usermod target.
-target_link_libraries(usermod INTERFACE picogame PicoGame_sdk)
+target_link_libraries(usermod INTERFACE picogame INTERFACE PicoGame_sdk)
--- module/picogame.c
+++ module/picogame.c
@@ -1,15 +1,29 @@
 #include "py/runtime.h"
 
-//#include "screen.h"
+#include "picoscreen.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+static mp_obj_t test_add(mp_obj_t x_obj, mp_obj_t y_obj) {
+	int x = mp_obj_get_int(x_obj);
+	int y= mp_obj_get_int(y_obj);
+
+	return mp_obj_new_int(x+y);
+}
+static MP_DEFINE_CONST_FUN_OBJ_2(test_add_obj, test_add);
+
+static mp_obj_t CreateScreen() {
+	return MP_OBJ_FROM_PTR(CreatePicoScreen());
+}
+static MP_DEFINE_CONST_FUN_OBJ_0(create_screen_obj, CreateScreen);
+
 static const mp_rom_map_elem_t PicoGame_globals_table[] = {
 	{ MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_PicoGame) },
 	//{ MP_ROM_QSTR(MP_QSTR_PicoMath), MP_ROM_PTR(&type_PicoMath) },
-	//{ MP_ROM_QSTR(MP_QSTR_test_add), MP_ROM_PTR(&test_add_obj)},
+	{ MP_ROM_QSTR(MP_QSTR_test_add), MP_ROM_PTR(&test_add_obj)},
+	{ MP_ROM_QSTR(MP_QSTR_CreateScreen), MP_ROM_PTR(&create_screen_obj)},
 };
 static MP_DEFINE_CONST_DICT(PicoGame_globals, PicoGame_globals_table);
 
