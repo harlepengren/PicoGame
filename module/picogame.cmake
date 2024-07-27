7fb676c59554821af9222dfc4418463bc7c0c05c
kkoehler4070 <koehler.62@gmail.com>
2024-07-27 14:50:06 -0400

    Update picogame.cmake
    Sd_card has an used variable. Since it is a submodule, I will not change directly. Instead, I remove the unused variable error check for the file.

--- module/picogame.cmake
+++ module/picogame.cmake
@@ -13,6 +13,9 @@ target_sources(picogame INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/FatFs_SPI/src/my_debug.c
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/FatFs_SPI/sd_driver/sd_card.c)
 
+set_source_files_properties(${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/FatFs_SPI/sd_driver/sd_card.c 
+    PROPERTIES COMPILE_OPTIONS "-Wno-unused-parameter;-Wno-missing-field-initializer")
+
 target_include_directories(picogame INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/
