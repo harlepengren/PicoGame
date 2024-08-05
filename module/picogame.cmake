b48a6bc740891443b54a9377b6a9434045881145
kkoehler4070 <koehler.62@gmail.com>
2024-08-05 06:20:44 -0400

    Update picogame.cmake
    Little i

--- module/picogame.cmake
+++ module/picogame.cmake
@@ -9,9 +9,9 @@ target_sources(picogame INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/image/image.c
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/image/hw_config.c
     #${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/FatFs_SPI/ff15/source/ff.c
-    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/src/src/f_util.c
+    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/src/f_util.c
     #${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/src/my_debug.c
-    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/sd_driver/sd_card.c)
+    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/sd_driver/sd_card.c)
 
 #set_source_files_properties(${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/FatFs_SPI/sd_driver/sd_card.c 
 #    PROPERTIES COMPILE_OPTIONS "-Wno-unused-parameter;-Wno-missing-field-initializer;-Wno-unused-variable")
@@ -24,8 +24,8 @@ target_include_directories(picogame INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/screen/
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/input
     ${CMAKE_CURRENT_LIST_DIR}/picogame_sdk/image
-    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/ff15/source/
-    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/sd_driver/
-    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPI-Pico/include)
+    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/ff15/source/
+    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/sd_driver/
+    ${CMAKE_CURRENT_LIST_DIR}/../no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/include)
 
 target_link_libraries(usermod INTERFACE picogame)
