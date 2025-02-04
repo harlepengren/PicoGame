c046b1003caf8109056e130abf8ca6564da9126b
kkoehler4070 <koehler.62@gmail.com>
2025-02-04 10:56:16 -0500

    Screen
    Moved ili9341.py into picogame for easier reference. Added a screen class to abstract the display functions/framebuffer.

--- picogame/screen.py
+++ picogame/screen.py
@@ -1,217 +1,25 @@
-from machine import Pin,SPI,PWM
+from ili9341 import Display, color565
 import framebuf
-import time
-<<<<<<< Updated upstream
+from machine import SPI, Pin
 
-BL = 13
-DC = 8
-RST = 12
-MOSI = 11
-SCK = 10    
-CS = 9
-
-class Screen(framebuf.FrameBuffer):
-    def __init__(self):
-        self.width = 128
-        self.height = 128
+class Screen:
+    def __init__(self, buffer_width, buffer_height, x=0, y=0):
+        try:
+            # Baud rate of 40000000 seems about the max
+            spi = SPI(1, baudrate=40000000, sck=Pin(14), mosi=Pin(15))
+            self._display = Display(spi, dc=Pin(13), cs=Pin(9), rst=Pin(10),rotation=0)
+            self._display.clear()
+            
+            self._x = x
+            self._y = y
+            self._width = buffer_width
+            self._height = buffer_height
+            self._buffer = framebuf.FrameBuffer(bytearray(buffer_width*buffer_height*2),buffer_width,buffer_height,framebuf.RGB565)
         
-        self.cs = Pin(CS,Pin.OUT)
-        self.rst = Pin(RST,Pin.OUT)
+        except KeyboardInterrupt:
+            self._display.cleanup()
+    
+    def clear(self,color):
+        self._buffer.fill(color)
+        self._display.block(self._x,self._y,self._width,self._height,self._buffer)
         
-        self.cs(1)
-        self.spi = SPI(1)
-        self.spi = SPI(1,1000_000)
-        self.spi = SPI(1,10000_000,polarity=0, phase=0,sck=Pin(SCK),mosi=Pin(MOSI),miso=None)
-        self.dc = Pin(DC,Pin.OUT)
-        self.dc(1)
-=======
-from . import ili9341
-
-SPI_NUM = 1
-CS = 9
-# BL = 13
-DC = 13
-RST = 10
-MOSI = 15
-CLK = 14
-
-
-class Screen(framebuf.FrameBuffer):
-    def __init__(self):
-        self.width = 240
-        self.height = 320
-        spi = SPI(SPI_NUM,baudrate=1000000,sck=Pin(CLK),mosi=Pin(MOSI))
-        self.display = ili9341.Display(SPI,Pin(CS),Pin(DC),Pin(RST))
-
->>>>>>> Stashed changes
-        self.buffer = bytearray(self.height * self.width * 2)
-        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
-        self.init_display()
-        
-
-        self.WHITE  =   0xFFFF
-        self.BLACK  =  0x0000
-        self.GREEN  =  0x001F
-        self.RED    =  0xF800
-        self.BLUE   = 0x07E0
-        self.GBLUE = 0X07FF
-        self.YELLOW = 0xFFE0
-        
-    def write_cmd(self, cmd):    
-        self.cs(1)
-        self.dc(0)
-        self.cs(0)
-        self.spi.write(bytearray([cmd]))
-        self.cs(1)
-
-    def write_data(self, buf):
-        self.cs(1)
-        self.dc(1)
-        self.cs(0)
-        self.spi.write(bytearray([buf]))
-        self.cs(1)
-
-    def init_display(self):
-        """Initialize dispaly"""  
-        self.rst(1)
-        self.rst(0)
-        self.rst(1)
-        
-        self.write_cmd(0x36);
-        self.write_data(0x70);
-        
-        self.write_cmd(0x3A);
-        self.write_data(0x05);
-
-         #ST7735R Frame Rate
-        self.write_cmd(0xB1);
-        self.write_data(0x01);
-        self.write_data(0x2C);
-        self.write_data(0x2D);
-
-        self.write_cmd(0xB2);
-        self.write_data(0x01);
-        self.write_data(0x2C);
-        self.write_data(0x2D);
-
-        self.write_cmd(0xB3);
-        self.write_data(0x01);
-        self.write_data(0x2C);
-        self.write_data(0x2D);
-        self.write_data(0x01);
-        self.write_data(0x2C);
-        self.write_data(0x2D);
-
-        self.write_cmd(0xB4); #Column inversion
-        self.write_data(0x07);
-
-        #ST7735R Power Sequence
-        self.write_cmd(0xC0);
-        self.write_data(0xA2);
-        self.write_data(0x02);
-        self.write_data(0x84);
-        self.write_cmd(0xC1);
-        self.write_data(0xC5);
-
-        self.write_cmd(0xC2);
-        self.write_data(0x0A);
-        self.write_data(0x00);
-
-        self.write_cmd(0xC3);
-        self.write_data(0x8A);
-        self.write_data(0x2A);
-        self.write_cmd(0xC4);
-        self.write_data(0x8A);
-        self.write_data(0xEE);
-
-        self.write_cmd(0xC5); #VCOM
-        self.write_data(0x0E);
-
-        #ST7735R Gamma Sequence
-        self.write_cmd(0xe0);
-        self.write_data(0x0f);
-        self.write_data(0x1a);
-        self.write_data(0x0f);
-        self.write_data(0x18);
-        self.write_data(0x2f);
-        self.write_data(0x28);
-        self.write_data(0x20);
-        self.write_data(0x22);
-        self.write_data(0x1f);
-        self.write_data(0x1b);
-        self.write_data(0x23);
-        self.write_data(0x37);
-        self.write_data(0x00);
-        self.write_data(0x07);
-        self.write_data(0x02);
-        self.write_data(0x10);
-
-        self.write_cmd(0xe1);
-        self.write_data(0x0f);
-        self.write_data(0x1b);
-        self.write_data(0x0f);
-        self.write_data(0x17);
-        self.write_data(0x33);
-        self.write_data(0x2c);
-        self.write_data(0x29);
-        self.write_data(0x2e);
-        self.write_data(0x30);
-        self.write_data(0x30);
-        self.write_data(0x39);
-        self.write_data(0x3f);
-        self.write_data(0x00);
-        self.write_data(0x07);
-        self.write_data(0x03);
-        self.write_data(0x10);
-
-        self.write_cmd(0xF0); #Enable test command
-        self.write_data(0x01);
-
-        self.write_cmd(0xF6); #Disable ram power save mode
-        self.write_data(0x00);
-            #sleep out
-        self.write_cmd(0x11);
-        #Turn on the LCD display
-        self.write_cmd(0x29);
-
-    def show(self):
-        self.write_cmd(0x2A)
-        self.write_data(0x00)
-        self.write_data(0x01)
-        self.write_data(0x00)
-        self.write_data(0x80)
-        
-        self.write_cmd(0x2B)
-        self.write_data(0x00)
-        self.write_data(0x02)
-        self.write_data(0x00)
-        self.write_data(0x82)
-        
-        self.write_cmd(0x2C)
-        
-        self.cs(1)
-        self.dc(1)
-        self.cs(0)
-        self.spi.write(self.buffer)
-        self.cs(1)
-
-    def clear(self):
-        self.fill(self.BLACK)
-        self.show()
-
-    def set_background_color(self,color):
-        self.fill(color)
-        self.show()
-
-    def get_color(self,color):
-        """Convert from RGB to RGB565 hex. The input argument color is a tuple (R, G, B)"""
-        # note that because we are going from 24 bits to 16 bits, there will be significant color loss
-
-
-        # The framebuf seems to flip the blue and green
-        red = ((color[0] >> 3) & 0x1f) << 11
-        blue = ((color[2] >> 3) & 0x1f) << 6
-        green = ((color[1] >> 2) & 0x3f)
-
-        return (red | green | blue)
-
